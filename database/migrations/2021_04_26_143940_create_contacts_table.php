05728b8523f9b0c4c9e7744ea429a9c4f2e39ca7
smart0120 <gadrgon0305@gmail.com>
2021-04-26 11:54:00 -0300

    feat: add contact model & migration

--- database/migrations/2021_04_26_143940_create_contacts_table.php
+++ database/migrations/2021_04_26_143940_create_contacts_table.php
@@ -15,6 +15,9 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unique('email');
+            $table->string('address');
             $table->timestamps();
         });
     }
